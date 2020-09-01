081be13d3b3495a040a7007d92fd032d75250b38
daniel2231 <kangheon2231@gmail.com>
2020-09-01 11:00:53 +0900

    hotfix GA

--- pages/_document.js
+++ pages/_document.js
@@ -30,6 +30,7 @@ export default class MyDocument extends Document {
               <script
                 dangerouslySetInnerHTML={{
                   __html: `
+
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
