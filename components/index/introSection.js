f9695415f5920f89bc1a977ae82eff10f93036ba
Daniel2231 <daniel2231.dev@gmail.com>
2022-08-22 21:13:57 +0900

    fixed intro

--- components/index/introSection.js
+++ components/index/introSection.js
@@ -1,199 +1,202 @@
-import styled from "styled-components";
-import { Col, Row } from "antd";
+import styled from 'styled-components';
+import { Col, Row } from 'antd';
 
 const Paragraph = styled.p`
-  font-size: 20px;
-  font-weight: lighter;
+	font-size: 20px;
+	font-weight: lighter;
 `;
 
 const ColImage = styled.img`
-  width: 20vh;
-  margin-bottom: 40px;
-  margin-right: 50px;
+	width: 20vh;
+	margin-bottom: 40px;
+	margin-right: 50px;
 `;
 
 function IntroSection() {
-  return (
-    <div style={{ height: "auto" }}>
-      <Row
-        style={{ marginBottom: "100px" }}
-        justify="space-around"
-        align="middle"
-        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
-      >
-        <Col
-          className="gutter-row"
-          xs={24}
-          md={8}
-          align="middle"
-          style={{ padding: "0% auto" }}
-        >
-          <ColImage src="../1.svg" />
-        </Col>
-        <Col className="gutter-row" xs={24} md={16}>
-          <h1>사무국</h1>
-          <Paragraph>
-            사무국은 전체적인 동아리 활동에 필요한 상시서류들을 작성하고,
-            신규등록 및 재등록 관련 업무를 진행합니다. 회장단을 보조하여 징계
-            대상 동아리를 관리하고, 동아리연합회 집행부 회의, 전동대회,
-            운영위원회 회의에 참석하여 회의록을 작성합니다.
-          </Paragraph>
-        </Col>
-      </Row>
-      <Row
-        style={{ marginBottom: "100px" }}
-        justify="space-around"
-        align="middle"
-        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
-      >
-        <Col
-          className="gutter-row"
-          xs={24}
-          md={8}
-          align="middle"
-          style={{ padding: "0% auto" }}
-        >
-          <ColImage src="../2.svg" />
-        </Col>
-        <Col className="gutter-row" xs={24} md={16}>
-          <h1>사무국 총무팀</h1>
-          <Paragraph>
-            사무국 소속 총무팀은 성균관대학교 중앙동아리의 원활한 활동을
-            보조하기 위해 동아리연합회 전반의 재정 관련 업무를 수행합니다.
-            경상보조금, 회장장학금, 행사지원금의 지급 관련 행정을 담당합니다.
-            또한 동아리연합회 사업 운영을 위한 학생회비 예산 수립, 집행, 학기별
-            결산도 담당하고 있습니다.
-          </Paragraph>
-        </Col>
-      </Row>
-      <Row
-        style={{ marginBottom: "100px" }}
-        justify="space-around"
-        align="middle"
-        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
-      >
-        <Col
-          className="gutter-row"
-          xs={24}
-          md={8}
-          align="middle"
-          style={{ padding: "0% auto" }}
-        >
-          <ColImage src="../3.svg" />
-        </Col>
-        <Col className="gutter-row" xs={24} md={16}>
-          <h1>대내사업국</h1>
-          <Paragraph>
-            대내사업국은 동아리연합회 내부운영에 초점을 맞추어 동아리인의
-            복지사업을 담당합니다. 동아리 운영의 편의와 효율을 고려하여 사업을
-            기획하여 원활한 활동이 진행될 수 있도록 지원합니다.
-          </Paragraph>
-        </Col>
-      </Row>
-      <Row
-        style={{ marginBottom: "100px" }}
-        justify="space-around"
-        align="middle"
-        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
-      >
-        <Col
-          className="gutter-row"
-          xs={24}
-          md={8}
-          align="middle"
-          style={{ padding: "0% auto" }}
-        >
-          <ColImage src="../4.svg" />
-        </Col>
-        <Col className="gutter-row" xs={24} md={16}>
-          <h1>행사기획국</h1>
-          <Paragraph>
-            행사기획국은 동아리연합회와 관련된 행사 혹은 대외적인 사업 전반의
-            진행과 기획을 담당합니다. 중앙동아리의 더 나은 활동을 보장하기
-            위하여 사업을 기획하고, 그 과정에서 다양한 아이디어를 제시합니다.
-          </Paragraph>
-        </Col>
-      </Row>
-      <Row
-        style={{ marginBottom: "100px" }}
-        justify="space-around"
-        align="middle"
-        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
-      >
-        <Col
-          className="gutter-row"
-          xs={24}
-          md={8}
-          align="middle"
-          style={{ padding: "0% auto" }}
-        >
-          <ColImage src="../5.svg" />
-        </Col>
+	return (
+		<div style={{ height: 'auto' }}>
+			<Row
+				style={{ marginBottom: '100px' }}
+				justify="space-around"
+				align="middle"
+				gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+			>
+				<Col
+					className="gutter-row"
+					xs={24}
+					md={8}
+					align="middle"
+					style={{ padding: '0% auto' }}
+				>
+					<ColImage src="../1.svg" />
+				</Col>
+				<Col className="gutter-row" xs={24} md={16}>
+					<h1>사무국</h1>
+					<Paragraph>
+						사무국은 전체적인 동아리 활동에 필요한 상시서류를 작성하고, 신규등록
+						및 재등록 관련 업무를 진행합니다. 징계 및 경고 동아리를 관리하고,
+						동아리연합회 집행부회의, 전체동아리대표자회의 및 운영위원회의에
+						참석하여 회의록을 작성하고 관리합니다.
+					</Paragraph>
+				</Col>
+			</Row>
+			<Row
+				style={{ marginBottom: '100px' }}
+				justify="space-around"
+				align="middle"
+				gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+			>
+				<Col
+					className="gutter-row"
+					xs={24}
+					md={8}
+					align="middle"
+					style={{ padding: '0% auto' }}
+				>
+					<ColImage src="../2.svg" />
+				</Col>
+				<Col className="gutter-row" xs={24} md={16}>
+					<h1>사무국 총무팀</h1>
+					<Paragraph>
+						사무국 소속 총무팀은 성균관대학교 중앙동아리의 원활한 활동을
+						보조하기 위해 동아리연합회 전반의 재정 관련 업무를 수행합니다.
+						경상보조금, 회장장학금, 행사지원금의 지급 관련 행정을 전담합니다.
+						또한 동아리연합회 사업 운영을 위한 학생회비 예산 수립, 집행, 학기별
+						결산을 전문적으로 담당합니다.
+					</Paragraph>
+				</Col>
+			</Row>
+			<Row
+				style={{ marginBottom: '100px' }}
+				justify="space-around"
+				align="middle"
+				gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+			>
+				<Col
+					className="gutter-row"
+					xs={24}
+					md={8}
+					align="middle"
+					style={{ padding: '0% auto' }}
+				>
+					<ColImage src="../7.svg" />
+				</Col>
 
-        <Col className="gutter-row" xs={24} md={16}>
-          <h1>홍보국</h1>
-          <Paragraph>
-            홍보국은 동아리연합회의 홍보와 문의답변에 관련된 업무를 담당합니다.
-            동아리연합회에서 진행하는 각종 행사들과 중앙동아리의 소식들을
-            종합하여 홍보합니다. 또한 SNS 계정을 관리하며 동아리 관련 문의에
-            답변하는 소통창구의 역할도 수행합니다. 홍보국 내에는 콘텐츠 제작을
-            위한 디자인팀이 소속되어 있습니다.
-          </Paragraph>
-        </Col>
-      </Row>
-      <Row
-        style={{ marginBottom: "100px" }}
-        justify="space-around"
-        align="middle"
-        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
-      >
-        <Col
-          className="gutter-row"
-          xs={24}
-          md={8}
-          align="middle"
-          style={{ padding: "0% auto" }}
-        >
-          <ColImage src="../6.svg" />
-        </Col>
+				<Col className="gutter-row" xs={24} md={16}>
+					<h1>비서 소통국</h1>
+					<Paragraph>
+						비서소통국은 회장단이 원활하게 업무를 수행할 수 있도록 회장단을
+						보조하며, 본회 소속 중앙동아리 대표자와의 소통과 SKKLUB 웹사이트
+						관리 등을 담당합니다. 중앙동아리 대표자와 동아리연합회 사이 징검다리
+						역할을 하며, 고충을 접수하고 문제를 해결하기 위해 노력합니다.
+						공용공간 대여사업 및 복지물품 대여사업을 진행하며, 익명 소통함을
+						관리하여 회장단에게 내용을 전달합니다.
+					</Paragraph>
+				</Col>
+			</Row>
+			<Row
+				style={{ marginBottom: '100px' }}
+				justify="space-around"
+				align="middle"
+				gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+			>
+				<Col
+					className="gutter-row"
+					xs={24}
+					md={8}
+					align="middle"
+					style={{ padding: '0% auto' }}
+				>
+					<ColImage src="../3.svg" />
+				</Col>
+				<Col className="gutter-row" xs={24} md={16}>
+					<h1>대내사업국</h1>
+					<Paragraph>
+						대내사업국은 동아리연합회 내부운영에 초점을 맞추어 동아리인의
+						복지사업을 담당합니다. 동아리 운영의 편의와 효율을 고려하여 사업을
+						기획하고, 원활한 활동이 이루어질 수 있도록 소속 중앙동아리를
+						지원합니다.
+					</Paragraph>
+				</Col>
+			</Row>
+			<Row
+				style={{ marginBottom: '100px' }}
+				justify="space-around"
+				align="middle"
+				gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+			>
+				<Col
+					className="gutter-row"
+					xs={24}
+					md={8}
+					align="middle"
+					style={{ padding: '0% auto' }}
+				>
+					<ColImage src="../4.svg" />
+				</Col>
+				<Col className="gutter-row" xs={24} md={16}>
+					<h1>행사기획국</h1>
+					<Paragraph>
+						행사기획국은 동아리연합회와 관련된 행사나 대외적인 사업 전반의
+						기획과 진행 전 과정을 담당합니다. 중앙동아리의 더 나은 활동을
+						보장하기 위해 사업을 기획하고, 그 과정에서 다양한 아이디어를
+						제시합니다. 동아리 홍보부스와 동아리의 밤 행사 등을 직접 수행합니다.
+					</Paragraph>
+				</Col>
+			</Row>
+			<Row
+				style={{ marginBottom: '100px' }}
+				justify="space-around"
+				align="middle"
+				gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+			>
+				<Col
+					className="gutter-row"
+					xs={24}
+					md={8}
+					align="middle"
+					style={{ padding: '0% auto' }}
+				>
+					<ColImage src="../5.svg" />
+				</Col>
 
-        <Col className="gutter-row" xs={24} md={16}>
-          <h1>홍보국 디자인팀</h1>
-          <Paragraph>
-            홍보국 소속 디자인팀은 팀장과 팀원으로 구성돼 있으며 홍보책자,
-            카드뉴스, 홍보물 디자인 등 전반적인 시각물 디자인 업무를 담당하고
-            있습니다.
-          </Paragraph>
-        </Col>
-      </Row>
-      <Row
-        style={{ marginBottom: "100px" }}
-        justify="space-around"
-        align="middle"
-        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
-      >
-        <Col
-          className="gutter-row"
-          xs={24}
-          md={8}
-          align="middle"
-          style={{ padding: "0% auto" }}
-        >
-          <ColImage src="../7.svg" />
-        </Col>
+				<Col className="gutter-row" xs={24} md={16}>
+					<h1>홍보국</h1>
+					<Paragraph>
+						홍보국은 동아리연합회의 홍보와 문의답변과 관련된 업무를 담당합니다.
+						동아리연합회에서 진행하는 각종 행사들과 중앙동아리의 소식을 종합하여
+						홍보합니다. SNS 계정을 관리하며, 동아리 관련 문의에 답변하는
+						소통창구의 역할도 겸행합니다.
+					</Paragraph>
+				</Col>
+			</Row>
+			<Row
+				style={{ marginBottom: '100px' }}
+				justify="space-around"
+				align="middle"
+				gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+			>
+				<Col
+					className="gutter-row"
+					xs={24}
+					md={8}
+					align="middle"
+					style={{ padding: '0% auto' }}
+				>
+					<ColImage src="../6.svg" />
+				</Col>
 
-        <Col className="gutter-row" xs={24} md={16}>
-          <h1>소통국</h1>
-          <Paragraph>
-            소통국은 동아리연합회의 공식적인 소통창구 운영, SKKLUB 웹사이트 운영
-            및 학생회관 내 자치공간 대여사업 등을 진행하고 보조합니다.
-            중앙동아리와 동아리연합회 간 징검다리의 역할을 하며 고충 및
-            문제해결을 위하여 힘쓰고 있습니다.
-          </Paragraph>
-        </Col>
-      </Row>
-    </div>
-  );
+				<Col className="gutter-row" xs={24} md={16}>
+					<h1>홍보국 디자인팀</h1>
+					<Paragraph>
+						홍보국 소속 디자인팀은 동아리 홍보책자, 카드뉴스와 영상을 포함한 SNS
+						콘텐츠 일체를 직접 디자인하고 이를 활용해 홍보사업을 수행합니다.
+						시각적인 디자인 업무를 전문적으로 담당합니다.
+					</Paragraph>
+				</Col>
+			</Row>
+		</div>
+	);
 }
 
 export default IntroSection;
--- components/index/secondContainer.js
+++ components/index/secondContainer.js
@@ -1,146 +1,147 @@
-import { Row, Col } from "antd";
-import styled from "styled-components";
-import QueueAnim from "rc-queue-anim";
-import { OverPack } from "rc-scroll-anim";
+import { Row, Col } from 'antd';
+import styled from 'styled-components';
+import QueueAnim from 'rc-queue-anim';
+import { OverPack } from 'rc-scroll-anim';
 
 const DivContainer = styled.div`
-  padding: 150px 60px 150px 60px;
-  background-color: #f8f8f9;
-  @media only screen and (max-width: 600px) {
-    height: 1400px;
-  }
+	padding: 150px 60px 150px 60px;
+	background-color: #f8f8f9;
+	@media only screen and (max-width: 600px) {
+		height: 1400px;
+	}
 
-  @media (min-width: 600px) {
-    height: 1400px;
-  }
-  @media (min-width: 768px) {
-    height: 1100px;
-  }
-  @media only screen and (min-width: 992px) {
-    height: 1150px;
-  }
+	@media (min-width: 600px) {
+		height: 1400px;
+	}
+	@media (min-width: 768px) {
+		height: 1100px;
+	}
+	@media only screen and (min-width: 992px) {
+		height: 1150px;
+	}
 `;
 
 const StyledRow = styled(Row)`
-  padding-bottom: 80px;
-  padding-left: 30px;
-  padding-right: 30px;
-  @media (max-width: 768px) {
-    padding-left: 0px;
-    padding-right: 0px;
-  }
+	padding-bottom: 80px;
+	padding-left: 30px;
+	padding-right: 30px;
+	@media (max-width: 768px) {
+		padding-left: 0px;
+		padding-right: 0px;
+	}
 `;
 
 const FirstColumn = styled(Col)`
-  text-align: right;
-  padding-bottom: 10px;
-  padding-right: 170px;
-  @media (max-width: 985px) {
-    text-align: left;
-    padding-right: 0px;
-  }
+	text-align: right;
+	padding-bottom: 10px;
+	padding-right: 170px;
+	@media (max-width: 985px) {
+		text-align: left;
+		padding-right: 0px;
+	}
 `;
 
 const LeftText = styled.h1`
-  font-weight: lighter;
-  @media (max-width: 768px) {
-    font-size: 1.7em;
-  }
+	font-weight: lighter;
+	@media (max-width: 768px) {
+		font-size: 1.7em;
+	}
 `;
 
 const SecondColumn = styled(Col)`
-  text-align: left;
+	text-align: left;
 `;
 
 const RightTextTitle = styled.h1`
-  @media (max-width: 768px) {
-    font-size: 1.3em;
-  }
+	@media (max-width: 768px) {
+		font-size: 1.3em;
+	}
 `;
 
 const RightTextParagraph = styled.p`
-  font-size: 1.3em;
-  font-weight: lighter;
-  word-break: keep-all;
-  @media (max-width: 768px) {
-    font-size: 1.1em;
-  }
+	font-size: 1.3em;
+	font-weight: lighter;
+	word-break: keep-all;
+	@media (max-width: 768px) {
+		font-size: 1.1em;
+	}
 `;
 
-console.log("height:", StyledRow.offsetHeight)
+console.log('height:', StyledRow.offsetHeight);
 
 function SecondContainer() {
-  return (
-    <div>
-      <DivContainer>
-        <OverPack always={false} style={{ height: StyledRow.offsetHeight }}>
-          <QueueAnim
-            className="queue-simple"
-            ease={["easeOutQuart", "easeInOutQuart"]}
-          >
-            <StyledRow key="a">
-              <FirstColumn xs={24} lg={10}>
-                <LeftText>Who we are</LeftText>
-              </FirstColumn>
-              <SecondColumn xs={24} lg={14}>
-                <RightTextTitle>
-                  안녕하세요,
-                  <br />
-                  동아리연합회 for:동 입니다!
-                </RightTextTitle>
-                <RightTextParagraph>
-                  성균관대학교 동아리연합회는 민주적인 절차를 통해 우리 대학
-                  캠퍼스 문화의 건전한 발 전을 위한 기틀을 마련하며 동아리 관련
-                  행정 처리 및 행사 추진 등을 담당합니다. 동아리 연합회는
-                  중앙동아리 소속 학우들의 활동을 장려하는 것을 넘어서, 교내
-                  모든 동아리 및 소모임, 학회 등이 이용할 수 있는 공간을
-                  제공하고, 중앙동아리 관련 정보를 신입생에게 제공하는 등
-                  성균인의 문화생활을 장려하고 있습니다.
-                </RightTextParagraph>
-              </SecondColumn>
-            </StyledRow>
-            <StyledRow key="b">
-              <FirstColumn xs={24} lg={10}>
-                <LeftText>What we do</LeftText>
-              </FirstColumn>
-              <SecondColumn xs={24} lg={14}>
-                <RightTextTitle>저희는 이런 활동을 해요!</RightTextTitle>
-                <RightTextParagraph>
-                  민주적 의견 수렴을 위해 상시로 운영위원회와 전동대회를 열어
-                  정보를 공유하고, 결정사 항을 함께 결정하며, 건의사항에 대해
-                  검토합니다. 소통창구를 통해 항상 학내 구성원의 목소리에 귀
-                  기울이고 있습니다. 동아리연합회는 특별기구로써
-                  중앙운영위원회를 구성, 학생자치활동에 적극적으로 참여합니다.
-                  학내 모든 동아리인들의 목소리를 대변하고, 서 로 다른 목소리,
-                  서로 다른 의견을 한데 모아 함께 논의하는 과정에서 최선을 향해
-                  나아가 려고 노력합니다. 동아리연합회는 성균관대학교의
-                  학생사회의 발전을 위해 이바지합니다.
-                </RightTextParagraph>
-              </SecondColumn>
-            </StyledRow>
-            <StyledRow key="c">
-              <FirstColumn xs={24} lg={10}>
-                <LeftText>How we work</LeftText>
-              </FirstColumn>
-              <SecondColumn xs={24} lg={14}>
-                <RightTextTitle>
-                  동아리연합회는 이렇게 구성되어 있어요!
-                </RightTextTitle>
-                <RightTextParagraph>
-                  동아리연합회는 원활한 업무 수행을 위해 5개의 집행부서를
-                  구성하여 운영하고 있습니다. 기획국, 총무국, 사무국, 편집국,
-                  홍보국이 서로 소통하고 업무를 함께 추진하며 행정 처리 를
-                  진행하고 있습니다. 이렇게 진행된 상시사업과 공약사업의 이행은
-                  학우들에게 투명하 게 공개되고 있습니다. 각국의 업무는 다음과
-                  같습니다. 투명하게 공개되고 있습니다.
-                </RightTextParagraph>
-              </SecondColumn>
-            </StyledRow>
-          </QueueAnim>
-        </OverPack>
-      </DivContainer>
-    </div>
-  );
+	return (
+		<div>
+			<DivContainer>
+				<OverPack always={false} style={{ height: StyledRow.offsetHeight }}>
+					<QueueAnim
+						className="queue-simple"
+						ease={['easeOutQuart', 'easeInOutQuart']}
+					>
+						<StyledRow key="a">
+							<FirstColumn xs={24} lg={10}>
+								<LeftText>Who we are</LeftText>
+							</FirstColumn>
+							<SecondColumn xs={24} lg={14}>
+								<RightTextTitle>
+									안녕하세요,
+									<br />
+									동아리연합회 for:동 입니다!
+								</RightTextTitle>
+								<RightTextParagraph>
+									성균관대학교 동아리연합회는 민주적인 절차를 통해 우리 대학
+									캠퍼스 문화의 건전한 발전을 위한 기틀을 마련하며, 동아리 관련
+									행정처리 및 행사 추진을 담당합니다. 동아리연합회는 중앙동아리
+									소속 학우들의 활동을 장려하고 지원하는 것을 넘어서, 교내 모든
+									동아리, 소모임, 학회 등이 이용할 수 있는 공간을 제공하고,
+									중앙동아리 관련 정보를 신입생에게 가장 먼저 제공하는 등 성균인
+									전체의 동아리 문화생활을 장려합니다.
+								</RightTextParagraph>
+							</SecondColumn>
+						</StyledRow>
+						<StyledRow key="b">
+							<FirstColumn xs={24} lg={10}>
+								<LeftText>What we do</LeftText>
+							</FirstColumn>
+							<SecondColumn xs={24} lg={14}>
+								<RightTextTitle>저희는 이런 활동을 해요!</RightTextTitle>
+								<RightTextParagraph>
+									민주적 의견 수렴을 위해 상시로 운영위원회와
+									전체동아리대표자회의를 개최하여 상호 정보를 공유하고,
+									의결사항을 결정하고, 건의 및 논의안건에 대해 의견을 나눕니다.
+									소통창구를 통해 항상 학내 구성원의 목소리에 귀를 기울이고
+									있습니다. 동아리연합회는 특별기구로 중앙운영위원회를 구성하고,
+									학생자치활동에 적극적으로 참여합니다. 학내 모든 동아리인의
+									목소리를 대변하고, 서로 다른 목소리와 의견을 한데 모아 함께
+									논의하는 과정을 통해 최선을 향해 나아가려 노력합니다.
+									동아리연합회는 성균관대학교 전체 학생사회의 발전을 위해
+									불철주야 노력하고 있습니다.
+								</RightTextParagraph>
+							</SecondColumn>
+						</StyledRow>
+						<StyledRow key="c">
+							<FirstColumn xs={24} lg={10}>
+								<LeftText>How we work</LeftText>
+							</FirstColumn>
+							<SecondColumn xs={24} lg={14}>
+								<RightTextTitle>
+									동아리연합회는 이렇게 구성되어 있어요!
+								</RightTextTitle>
+								<RightTextParagraph>
+									동아리연합회는 원활한 업무수행을 위해 5개의 집행국과 2개의
+									특별사무팀으로 구성됩니다. 대내사업국, 비서소통국, 사무국,
+									행사기획국, 홍보국의 5국과 총무팀, 디자인팀의 2팀이 서로
+									소통하며 업무를 함께 추진하고 있습니다. 이렇게 진행되는
+									상시사업과 공약사업의 이행과정은 학우들에게 투명하게
+									공개됩니다. 각 국의 업무는 아래와 같습니다.
+								</RightTextParagraph>
+							</SecondColumn>
+						</StyledRow>
+					</QueueAnim>
+				</OverPack>
+			</DivContainer>
+		</div>
+	);
 }
 
 export default SecondContainer;
